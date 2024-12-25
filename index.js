315d9aa954ecc313e432a47a16f0d56ccaad064e
barakyaari <barakyaari@gmail.com>
2024-12-25 10:49:58 +0700

    xx

--- index.js
+++ index.js
@@ -45,6 +45,11 @@ app.post('/getWebsiteContent', async (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
+
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
